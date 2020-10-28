c3995a826cc5da70237f5eb9f7c58ad51710f7f3
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-10-28 18:47:08 -0400

    added desired table setup with dummy data

--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -1,29 +1,14 @@
-import React, { Component } from "react";
-
-class Row extends Component {
-  render() {
-    return (
-      <>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td colspan="2">Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </>
-    );
-  }
-}
+const Row = () => {
+  return (
+    <tr>
+      <td><img src="http://www.fillmurray.com/50/75" alt="Bill Murray"/></td>
+      <td>Bill</td>
+      <td>Murray</td>
+      <td>bill@laughs.com</td>
+      <td>123-867-5309</td>
+      <td>09/21/1950</td>
+    </tr>
+  );
+};
 
 export default Row;
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,16 +1,22 @@
-import React, { Component } from "react";
 import Row from "../Row/Row"
+import React, { Component } from 'react';
 
 class Table extends Component {
+// state = {
+//     employees: []
+// }
+
   render() {
     return (
       <table className="table table-hover">
         <thead>
           <tr>
-            <th scope="col">#</th>
+            <th scope="col">Image</th>
             <th scope="col">First</th>
             <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone Number</th>
+            <th scope="col">Birth Date</th>
           </tr>
         </thead>
         <tbody>
