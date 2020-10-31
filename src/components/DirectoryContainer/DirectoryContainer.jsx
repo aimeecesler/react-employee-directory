154bc7f77ce8279656239e956dc340720a60e73b
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-10-30 22:55:35 -0400

    formatted header

--- src/components/DirectoryContainer/DirectoryContainer.jsx
+++ src/components/DirectoryContainer/DirectoryContainer.jsx
@@ -97,16 +97,20 @@ class DirectoryContainer extends Component {
       <>
         <div className="container-fluid">
           <div className="row main-header mb-5">
-            <div className="col-sm-12 text-center my-5">
-              <h1>Employee Directory</h1>
+            <div className="col-sm-12 my-auto">
+              <h1 className="ml-3" id="banner-header">Employee Directory</h1>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row mb-2">
+            <div className="col-sm-12">
               <h3>
                 Click on carrots to filter by heading or use the search box to
                 narrow your results.
               </h3>
             </div>
           </div>
-        </div>
-        <div className="container">
           <div className="row">
             <div className="col-sm-4"></div>
             <div className="col-sm-4">
